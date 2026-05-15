110fad5695367ccc0f3409c861f2706c5342e668
Mariya Shaji <mariyashaji08@gmail.com>
2026-05-15 12:32:15 +0530

    3. Add webcam feed
    Initialize video capture and update frame display.

--- posture_app.py
+++ posture_app.py
@@ -1,12 +1,16 @@
-class MainWindow(QWidget):
-    def __init__(self):
-        super().__init__()
+ self.cap = cv2.VideoCapture(0)
 
-        self.setWindowTitle("PostureGuard")
+self.timer = QTimer()
+self.timer.timeout.connect(self.update_frame)
+self.timer.start(30)
+def update_frame(self):
+    ret, frame = self.cap.read()
 
-        layout = QVBoxLayout()
+    if ret:
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        self.label = QLabel("Camera Loading...")
-        layout.addWidget(self.label)
+        h, w, ch = rgb.shape
 
-        self.setLayout(layout)
+        img = QImage(rgb.data, w, h, ch * w, QImage.Format_RGB888)
+
+        self.label.setPixmap(QPixmap.fromImage(img))
