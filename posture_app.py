f35142d260d062384a83d69be44340d25bfc82ed
Mariya Shaji <mariyashaji08@gmail.com>
2026-05-15 12:33:29 +0530

    5. Add Haar cascade detection

--- posture_app.py
+++ posture_app.py
@@ -1,3 +1,4 @@
- class PostureAnalyser:
-    def analyse(self, frame):
-        return "Good Posture", 0.90
+ self.face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+faces = self.face_cascade.detectMultiScale(gray, 1.1, 5)
