75a5e004ce09ea3780c605eeb19a32d81210302b
Mariya Shaji <mariyashaji08@gmail.com>
2026-05-15 12:32:41 +0530

    4. Add posture analyser class

--- posture_app.py
+++ posture_app.py
@@ -1,16 +1,3 @@
- self.cap = cv2.VideoCapture(0)
-
-self.timer = QTimer()
-self.timer.timeout.connect(self.update_frame)
-self.timer.start(30)
-def update_frame(self):
-    ret, frame = self.cap.read()
-
-    if ret:
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        h, w, ch = rgb.shape
-
-        img = QImage(rgb.data, w, h, ch * w, QImage.Format_RGB888)
-
-        self.label.setPixmap(QPixmap.fromImage(img))
+ class PostureAnalyser:
+    def analyse(self, frame):
+        return "Good Posture", 0.90
